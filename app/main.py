dbb6c73c8b66a5c3f5421ab5b3a996e3ac8c9441
aquiles23 <jjoseaquiless@gmail.com>
2022-06-02 12:04:43 -0300

    fix the solution - the solution is now giving the good results - comment a little of code - fix exception to throw a HTTPException instead of a generic Exception

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import re
 
@@ -8,8 +8,7 @@ app = FastAPI()
 
 class Payload(BaseModel):
     text: str
-    def __list__(self):
-        return self.text.split()
+
 
 romanNumbersDict = {
     "M": 1000,
@@ -19,40 +18,36 @@ romanNumbersDict = {
     "X": 10,
     "V": 5,
     "I": 1,
-    "IV": 4,
-    "IX": 9,
-    "XL": 40,
-    "XC": 90,
-    "CD": 400,
-    "CM": 900
 }
 
+
 def romanToInteger(romanNumber: str) -> int:
-    # convert romanNumber into integer
     number = 0
-    if len(romanNumber) == 1:
-        number = romanNumbersDict[romanNumber]
-    else:
-        for i in range(1, len(romanNumber)+1):
-            if romanNumbersDict[romanNumber[i]] > romanNumbersDict[romanNumber[i-1]]:
-                number += romanNumbersDict[romanNumber[i-1:i+1]]
-            else:
-                number += romanNumbersDict[romanNumber[i-1]]
+    for i, x in enumerate(romanNumber):
+        if i == len(romanNumber) - 1:
+            number += romanNumbersDict[x]
+            break
+        elif romanNumbersDict[x] < romanNumbersDict[romanNumber[i + 1]]:
+            number -= romanNumbersDict[x]
+        else:
+            number += romanNumbersDict[x]
     return number
 
-@app.get("/search")
-def welcome():
-    return 'Welcome'
 
 @app.post("/search")
-def max_roman_number(input: Payload = None) -> Union[str, dict]:    
+def max_roman_number(input: Payload) -> Union[str, dict]:
     if input.text[0] in romanNumbersDict:
-        raise Exception("there is no index")
-    
+        raise HTTPException(
+            status_code=400,
+            detail="the payload don't start with a index like 'A', 'B', 'E' etc. it start with a roman number",
+        )
+
     # separate roman numbers in a python list
     romanList = re.findall(r"[MDCLXVI]+", input.text)
 
     numbersList = [romanToInteger(romanNumber) for romanNumber in romanList]
-    
+
+    # create a list of tuples of numberList and romanList values
+    # and extract its max value based on numbersList value
     responseValues = max(zip(numbersList, romanList), key=lambda x: x[0])
     return {"number": responseValues[1], "value": responseValues[0]}
