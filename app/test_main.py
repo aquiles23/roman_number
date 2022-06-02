e7cb84b1c97a7da239f6d0d3b872642a5316f586
aquiles23 <jjoseaquiless@gmail.com>
2022-06-02 12:06:18 -0300

    more tests - remove welcome test because it was also removed from the solution - include tests that cover more edge cases - add exception test

--- app/test_main.py
+++ app/test_main.py
@@ -5,12 +5,23 @@ from .main import app
 client = TestClient(app)
 
 
+def test_exceptions():
+    response = client.post("/search", json={"text": "XXX"})
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "the payload don't start with a index like 'A', 'B', 'E' etc. it start with a roman number"
+    }
+
+
 @pytest.mark.parametrize(
     "test_input,expected",
     [
         ({"text": "AXXBLX"}, {"number": "LX", "value": 60}),
         ({"text": "AXXXBLXX"}, {"number": "LXX", "value": 70}),
-        (None, 'Welcome')
+        # mutiples index test
+        ({"text": "AXXXBLXXEMFCC"}, {"number": "M", "value": 1000}),
+        # test with values like IV, IX, XL etc
+        ({"text": "AXXIXBCCXCIV"}, {"number": "CCXCIV", "value": 294}),
     ],
 )
 def test_read_main(test_input, expected):
